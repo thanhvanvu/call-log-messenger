3276e515c773b4ff21e968a327a77a5494a7c2ed
Thanh Vu <vvt4994@gmail.com>
2025-02-21 07:47:46 -0600

    fix meta data

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -35,14 +35,7 @@ export const metadata: Metadata = {
       "Easily manage, analyze, and export call logs in a designed PDF format, supporting over 10 languages.",
     url: "https://call-log-messenger.vercel.app/",
     type: "website",
-    images: [
-      {
-        url: "https://i.imgur.com/mheSIaQ.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Call logs and statistics dashboard",
-      },
-    ],
+    images: ["https://i.imgur.com/mheSIaQ.jpg"],
   },
   twitter: {
     card: "summary_large_image",
