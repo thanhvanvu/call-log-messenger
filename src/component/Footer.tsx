204dbfb271fb355f45af061be24ce2259f12ab89
Thanh Vu <vvt4994@gmail.com>
2025-03-05 03:05:58 -0600

    Update Footer.tsx

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -5,7 +5,11 @@ export const Footer = () => {
   return (
     <div className="text-center bg-white text-sm py-3 text-[#6B7985]">
       Copyright © 2025{" "}
-      <a href="mailto:call.log.messenger@gmail.com" className="text-[blue] underline">
+      <a
+        href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=call.log.messenger@gmail.com"
+        target="_blank"
+        className="text-[blue] underline"
+      >
         call.log.messenger@gmail.com
       </a>{" "}
     </div>
