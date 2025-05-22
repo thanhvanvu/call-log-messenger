fe8c8b425c7333cb11276614384f3c446333b002
Thanh Vu <vvt4994@gmail.com>
2025-05-21 22:21:18 -0500

    improve SEO by updating robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "call-log-messenger.vercel.app/sitemap.xml",
+    sitemap: "https://call-log-messenger.vercel.app/sitemap.xml",
   };
 }
